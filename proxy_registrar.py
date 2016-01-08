be11eaee37201e5dde6fb4b17569cd30999328f6
beatrizcampos <beatrizcl.1404@gmail.com>
2016-01-08 23:57:10 +0100

    Comenzamos programa proxy

--- proxy_registrar.py
+++ proxy_registrar.py
@@ -15,3 +15,13 @@ except IndexError:
     print("Usage: python proxy_registrar.py config")
 
 print("Server ServidorFamilySimpson listening at port 5555")
+
+# Abrimos fichero xml para coger informacion
+fich = open(CONFIG, 'r')
+line = fich.readlines()
+fich.close()
+
+# NOMBRE ; PUERTO ; IP del Servidor donde escuchara
+line_sever = line[2].split(">")
+print(line_sever)
+
