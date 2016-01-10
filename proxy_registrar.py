eb1ae879aa3a1bb8f02dd62dcdf7f1362ea259aa
beatrizcampos <beatrizcl.1404@gmail.com>
2016-01-10 17:53:01 +0100

    METODO REGISTER completado con autenticacion (FALTA EXPIRE 0) - proxy

--- proxy_registrar.py
+++ proxy_registrar.py
@@ -8,6 +8,8 @@ Programa Proxy
 
 import sys
 import socketserver
+import hashlib
+import csv
 
 
 try:
@@ -41,12 +43,6 @@ class ProxyHandler(socketserver.DatagramRequestHandler):
 
             if method_client == "REGISTER":
                 linea_troceada = line.decode('utf-8').split(" ")
-                print(len(linea_troceada))
-                print(linea_troceada[0])
-                print(linea_troceada[1])
-                print(linea_troceada[2])
-                print(linea_troceada[3])
-                print(linea_troceada[-1])
 
                 if len(linea_troceada) == 4:
                     # Enviamos: SIP/2.0 401 Unauthorized
@@ -61,9 +57,35 @@ class ProxyHandler(socketserver.DatagramRequestHandler):
                         answer += "WWW Authenticate: nonce="
                         answer += str(nonce) + "\r\n\r\n"
                         self.wfile.write(bytes(answer, 'utf-8'))
-
-                  #else:
-                    # compreba el response
+                
+                else:
+                    # Comprobamos el response (si bien autentificado 200OK, sino 401 Unauthorized
+                    direccionsip_client2 = linea_troceada[1].split(':')[1]
+                    puerto_client2 = int(linea_troceada[1].split(':')[-1])
+                    expires = int(linea_troceada[3].split('\r\n')[0])
+                    
+                    response = linea_troceada[-1].split('=')[-1]
+                    response = response.split('\r')[0]
+                    print(direccionsip_client2)
+                    print(expires)
+                    print(response)
+                    m = hashlib.md5()
+                    for usuario in passwords_usuarios.keys():
+                        if usuario == direccionsip_client2:
+                            password = passwords_usuarios[usuario]
+                    print(password);
+                    
+                    m.update(bytes(password, 'utf-8'))
+                    m.update(bytes(str(nonce), 'utf-8'))   
+                    if m.hexdigest() == response:
+                        answer = "SIP/2.0 200 OK\r\n\r\n"
+                        print("MANDAMOS :\r\n", answer)
+                        self.wfile.write(bytes(answer, 'utf-8'))
+                    else:
+                        answer = "SIP/2.0 401 Unauthorized\r\n"
+                        answer += "WWW Authenticate: nonce="
+                        answer += str(nonce) + "\r\n\r\n"
+                        self.wfile.write(bytes(answer, 'utf-8'))       
 
         
 
@@ -90,10 +112,13 @@ if __name__ == "__main__":
     print("PUERTO DEL SERVIDOR:  ",PUERTO_SERVER)
 
     # Database
-    line_database = line[2].split(">")
+    line_database = line[2].split(">")    
     database = line_database[0].split("=")[1]
     PATH_DATABASE = database.split(" ")[0][1:-2]
     print(PATH_DATABASE)
+    path_password = line_database[0].split("=")[2]
+    PASSWORDS_DATABASE = path_password.split("=")[0][1:-2]
+    print(PASSWORDS_DATABASE)
 
     # Fichero Log
     line_log = line[3].split(">")
@@ -101,9 +126,16 @@ if __name__ == "__main__":
     PATH_LOGSERVER = log.split(" ")[0][1:-2]
     print(PATH_LOGSERVER)
 
-    # LEER FICHERO PASSWORD.TXT
-    # COGES LAS CONTRASEÑAS
-    
+    # LEER FICHERO PASSWORD.TXT , COGER CONTRASEÑAS
+    with open(PASSWORDS_DATABASE, newline='') as pwrd_fich:
+        lineas = csv.reader(pwrd_fich)
+        passwords_usuarios = {}
+        for linea in lineas:
+            linea_usuario = linea[0].split(':')
+            passwords_usuarios[linea_usuario[0]] = linea_usuario[-1]
+        print(passwords_usuarios)
+
+
     serv = socketserver.UDPServer(((IP_SERVER, int(PUERTO_SERVER))), ProxyHandler)
     print("Server " + NAME_SERVER +  " listening at port " + str(PUERTO_SERVER))
     serv.serve_forever()
