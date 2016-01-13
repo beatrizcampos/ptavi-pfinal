0d22c5667661f7dec946c0511443575a1a9bd7ed
beatrizcampos <beatrizcl.1404@gmail.com>
2016-01-13 02:02:32 +0100

    FUNCION PARA FICHERO LOG MEJORADA- cliente

--- uaclient.py
+++ uaclient.py
@@ -30,24 +30,26 @@ def hora_actual():
 def fich_log(fichero, evento, ip, puerto, texto):
 
     fich = open(fichero, 'a')
-    hora = txt.write(Get_Time())
+    hora = fich.write(hora_actual())
 
     if evento == "sent_to":
-        fich.write(hora + " Sent to " + ip
-                   + ":" + puerto + ":" + texto + "\r\n")
+        fich.write(" Sent to " + ip
+                   + ":" + str(puerto) + ":" + texto + "\r\n")
 
     elif evento == "received":
-        fich.write(hora + " Received from " + ip + ":"
-                   + puerto + ":" + texto + "\r\n")
+        fich.write(" Received from " + ip + ":"
+                   + str(puerto) + ":" + texto + "\r\n")
 
     elif evento == "error":
-        fich.write(hora + text + '\r\n')
+        fich.write(texto + '\r\n')
 
     elif evento == "starting":
-        fich.write(hora + " Starting...\r\n")
+        fich.write(" Starting... \r\n")
 
     elif evento == "finishing":
-        fich.write(hora + "Finishing. \r\n")
+        fich.write("Finishing. \r\n")
+
+    fich.close()
 
 
 if __name__ == "__main__":
@@ -55,123 +57,123 @@ if __name__ == "__main__":
     Cliente UDP simple.
     """
 
-# Abrimos fichero xml para coger informacion
-fich = open(CONFIG, 'r')
-line = fich.readlines()
-fich.close()
+    # Abrimos fichero xml para coger informacion
+    fich = open(CONFIG, 'r')
+    line = fich.readlines()
+    fich.close()
 
-#Conseguimos nombre de usuario y contraseña (linea 3-xml)
-line_account = line[3].split(">")
-account = line_account[0].split("=")[1]
-USERNAME = account.split(" ")[0][1:-1]
-print("imprimimos nombre:   ", USERNAME)
-passw = line_account[0].split("=")[2]
-PASSWORD = passw.split(" ")[0][1:-2]
-print("imprimimos contraseña:   ", PASSWORD)
-#IP
-line_uaserver = line[4].split(">")
-uaserver = line_uaserver[0].split("=")[1]
-IP = uaserver.split(" ")[0][1:-1]
-if not IP:
-    IP = "127.0.0.1"
-print("Imprimimos IP:   ", IP)
+    #Conseguimos nombre de usuario y contraseña (linea 3-xml)
+    line_account = line[3].split(">")
+    account = line_account[0].split("=")[1]
+    USERNAME = account.split(" ")[0][1:-1]
+    print("imprimimos nombre:   ", USERNAME)
+    passw = line_account[0].split("=")[2]
+    PASSWORD = passw.split(" ")[0][1:-2]
+    print("imprimimos contraseña:   ", PASSWORD)
+    #IP
+    line_uaserver = line[4].split(">")
+    uaserver = line_uaserver[0].split("=")[1]
+    IP = uaserver.split(" ")[0][1:-1]
+    if not IP:
+        IP = "127.0.0.1"
+    print("Imprimimos IP:   ", IP)
 
-#PUERTO
-uaserver_puerto = line_uaserver[0].split("=")[2]
-PUERTO = uaserver_puerto.split(" ")[0][1:-2]
-print("Imprimimos PUERTO:   ", PUERTO)
+    #PUERTO
+    uaserver_puerto = line_uaserver[0].split("=")[2]
+    PUERTO = uaserver_puerto.split(" ")[0][1:-2]
+    print("Imprimimos PUERTO:   ", PUERTO)
 
-#PUERTO RTP
-line_rtpaudio = line[5].split(">")
-rtpaudio = line_rtpaudio[0].split("=")[1]
-PUERTO_RTP = rtpaudio.split(" ")[0][1:-2]
-print("Imprimimos PUERTO RTP:   ", PUERTO_RTP)
+    #PUERTO RTP
+    line_rtpaudio = line[5].split(">")
+    rtpaudio = line_rtpaudio[0].split("=")[1]
+    PUERTO_RTP = rtpaudio.split(" ")[0][1:-2]
+    print("Imprimimos PUERTO RTP:   ", PUERTO_RTP)
 
-#IP y PUERTO DEL PROXY
-line_regproxy = line[6].split(">")
-regproxy = line_regproxy[0].split("=")[1]
-IP_PROXY = regproxy.split(" ")[0][1:-1]
-print("Imprimimos IP DEL Proxy:   ", IP_PROXY)
-regproxy_puerto = line_regproxy[0].split("=")[2]
-PUERTO_PROXY = regproxy_puerto.split(" ")[0][1:-2]
-print("Imprimimos Puerto DEL Proxy:   ", PUERTO_PROXY)
+    #IP y PUERTO DEL PROXY
+    line_regproxy = line[6].split(">")
+    regproxy = line_regproxy[0].split("=")[1]
+    IP_PROXY = regproxy.split(" ")[0][1:-1]
+    print("Imprimimos IP DEL Proxy:   ", IP_PROXY)
+    regproxy_puerto = line_regproxy[0].split("=")[2]
+    PUERTO_PROXY = regproxy_puerto.split(" ")[0][1:-2]
+    print("Imprimimos Puerto DEL Proxy:   ", PUERTO_PROXY)
 
+    #Localizacion Path log
+    line_log = line[7].split(">")
+    log = line_log[0].split("=")[1]
+    PATH_LOG = log.split(" ")[0][1:-2]
+    print(PATH_LOG)
 
-#Localizacion Path log
-line_log = line[7].split(">")
-log = line_log[0].split("=")[1]
-PATH_LOG = log.split(" ")[0][1:-2]
-print(PATH_LOG)
+    #Localizacion PAth Audio
+    line_audio = line[8].split(">")
+    audio = line_audio[0].split("=")[1]
+    PATH_AUDIO = audio.split(" ")[0][1:-2]
+    print(PATH_AUDIO)
 
-#Localizacion PAth Audio
-line_audio = line[8].split(">")
-audio = line_audio[0].split("=")[1]
-PATH_AUDIO = audio.split(" ")[0][1:-2]
-print(PATH_AUDIO)
+    # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
+    my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    my_socket.connect((IP_PROXY, int(PUERTO_PROXY)))
 
+    # Comenzamos a escribir fichero log 
+    texto = ""
+    fich_log(PATH_LOG, "starting", IP, PUERTO, texto)
 
-# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
-my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-my_socket.connect((IP_PROXY, int(PUERTO_PROXY)))
+    #METODO REGISTER
+    if METHOD == 'REGISTER':
+        LINE = METHOD + ' sip:' + USERNAME + ':' + PUERTO + ' SIP/2.0\r\n'
+        LINE += "Expires: " + OPTION + "\r\n"
 
+    elif METHOD == 'INVITE':
+        # Añadimos cabeceras
+        LINE = "INVITE " + "sip:" + OPTION + " SIP/2.0\r\n"
+        LINE += "Content-Type: application/sdp\r\n\r\n"
+        LINE += "v=0\r\n" + "o=" + USERNAME + " " + IP + " \r\n"
+        LINE += "s=SIP's PARTY" + "\r\n" + "t=0" + "\r\n"
+        LINE += "m=audio " + PUERTO_RTP + " RTP" + "\r\n"
 
-#METODO REGISTER
-if METHOD == 'REGISTER':
-    LINE = METHOD + ' sip:' + USERNAME + ':' + PUERTO + ' SIP/2.0\r\n'
-    LINE += "Expires: " + OPTION + "\r\n"
+    elif METHOD == 'BYE':
+        #BYE sip:receptor SIP/2.0
+        LINE = METHOD + " sip:" + OPTION + " SIP/2.0\r\n"
 
-elif METHOD == 'INVITE':
-    # Añadimos cabeceras
-    LINE = "INVITE " + "sip:" + OPTION + " SIP/2.0\r\n"
-    LINE += "Content-Type: application/sdp\r\n\r\n"
-    LINE += "v=0\r\n" + "o=" + USERNAME + " " + IP + " \r\n"
-    LINE += "s=SIP's PARTY" + "\r\n" + "t=0" + "\r\n"
-    LINE += "m=audio " + PUERTO_RTP + " RTP" + "\r\n"
-
-elif METHOD == 'BYE':
-    #BYE sip:receptor SIP/2.0
-    LINE = METHOD + " sip:" + OPTION + " SIP/2.0\r\n"
-
-# Enviamos la petición
-print("Enviando: \r\n" + LINE)
-my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
-# Recibimos respuesta
-data = my_socket.recv(1024)
-print("Recibido: \r\n", data.decode('utf-8'))
-
-
-# Estudiamos respuesta
-data = data.decode('utf-8').split("\r\n")
-
-if data[0] == "SIP/2.0 401 Unauthorized":
-    # Añadimos cabecera autenticación (FUNCION HASH)
-    m = hashlib.md5()
-    nonce = data[1].split("=")[-1]
-    print(nonce)
-    m.update(bytes(PASSWORD, 'utf-8'))
-    m.update(bytes(nonce, 'utf-8'))
-    LINE += "Authorization: response=" + m.hexdigest() + "\r\n"
+    # Enviamos la petición
     print("Enviando: \r\n" + LINE)
     my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+    # Recibimos respuesta
     data = my_socket.recv(1024)
-    print(data.decode('utf-8'))
+    print("Recibido: \r\n", data.decode('utf-8'))
 
-elif data[0] == "SIP/2.0 100 Trying":
-    # Metodo de asentimiento. ACK sip:receptor SIP/2.0
-    METHOD = 'ACK'
-    LINE = METHOD + ' sip:' + OPTION + ' SIP/2.0\r\n'
-    print("Enviando: \r\n" + LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
-    # Envio RTP
-    # aEjecutar es un string con lo que se ha de ejecutar en la shell
-    print(data[9])
+    # Estudiamos respuesta
+    data = data.decode('utf-8').split("\r\n")
 
-    IP_RTP = data[9].split(' ')[-1]
-    print(IP_RTP)
-    PUERTO_RTP = data[12].split(' ')[-2]
-    print(PUERTO_RTP)
-    aEjecutar = "./mp32rtp -i " + IP_RTP + " -p " + PUERTO_RTP
-    aEjecutar += " < " + PATH_AUDIO
-    print("Vamos a ejecutar", aEjecutar)
-    os.system(aEjecutar)
+    if data[0] == "SIP/2.0 401 Unauthorized":
+        # Añadimos cabecera autenticación (FUNCION HASH)
+        m = hashlib.md5()
+        nonce = data[1].split("=")[-1]
+        print(nonce)
+        m.update(bytes(PASSWORD, 'utf-8'))
+        m.update(bytes(nonce, 'utf-8'))
+        LINE += "Authorization: response=" + m.hexdigest() + "\r\n"
+        print("Enviando: \r\n" + LINE)
+        my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+        data = my_socket.recv(1024)
+        print(data.decode('utf-8'))
+
+    elif data[0] == "SIP/2.0 100 Trying":
+        # Metodo de asentimiento. ACK sip:receptor SIP/2.0
+        METHOD = 'ACK'
+        LINE = METHOD + ' sip:' + OPTION + ' SIP/2.0\r\n'
+        print("Enviando: \r\n" + LINE)
+        my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+        # Envio RTP
+        # aEjecutar es un string con lo que se ha de ejecutar en la shell
+        print(data[9])
+
+        IP_RTP = data[9].split(' ')[-1]
+        print(IP_RTP)
+        PUERTO_RTP = data[12].split(' ')[-2]
+        print(PUERTO_RTP)
+        aEjecutar = "./mp32rtp -i " + IP_RTP + " -p " + PUERTO_RTP
+        aEjecutar += " < " + PATH_AUDIO
+        print("Vamos a ejecutar", aEjecutar)
+        os.system(aEjecutar)
